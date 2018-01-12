d1e8cca1eafd2149fedf62774bf78c6a74309448
andrew <a@a-designer.ca>
2018-01-12 01:08:58 -0500

    new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { UserSelectionComponent } from './user-selection/user-selection.component';
 import { ThreadSectionComponent } from './thread-section/thread-section.component';
 import { MessageSectionComponent } from './message-section/message-section.component';
+import { MessageListComponent } from './message-list/message-list.component';
+import { ThreadListComponent } from './thread-list/thread-list.component';
 
 
 @NgModule({
@@ -13,7 +15,9 @@ import { MessageSectionComponent } from './message-section/message-section.compo
     AppComponent,
     UserSelectionComponent,
     ThreadSectionComponent,
-    MessageSectionComponent
+    MessageSectionComponent,
+    MessageListComponent,
+    ThreadListComponent
   ],
   imports: [
     BrowserModule
